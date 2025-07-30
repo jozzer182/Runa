40d2515a9cbe6f26da9a3c5e34c4a2cedded7c97
Jose Zarabanda <jlzarabandad@gmail.com>
2025-07-30 05:40:36 -0400

    feat: Initialize Runa application with main structure and assets

---
+++ apple/runa/runa/runaApp.swift
@@ -0,0 +1,17 @@
+//
+//  runaApp.swift
+//  runa
+//
+//  Created by JOSE ZARABANDA on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct runaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
